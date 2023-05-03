22bd3093beae850fd2be7b84e6b923dbacf16e20
phong tu <phong.tu@alpaca.vn>
2023-05-03 14:58:35 +0700

    Giao diện giỏ hàng thanh toán

--- app/src/main/java/com/example/gearshop/ui/giohang/CartListActivity.java
+++ app/src/main/java/com/example/gearshop/ui/giohang/CartListActivity.java
@@ -9,12 +9,23 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.gearshop.R;
+import com.example.gearshop.ui.cart.CartArrayAdapter;
+import com.example.gearshop.ui.cart.CartItem;
+
+import java.util.ArrayList;
 
 public class CartListActivity extends AppCompatActivity {
     Toolbar toolbar;
     Button btnThanhToanHoaDon;
+    TextView tongTientxt;
+    ArrayList<CartItem> cartItemsList;
+    CartArrayAdapter cartArrayAdapter;
+    ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,9 +46,30 @@ public class CartListActivity extends AppCompatActivity {
             }
         });
     }
+    private ArrayList<CartItem> createMockup () {
+        cartItemsList = new ArrayList<>();
 
+        cartItemsList.add(new CartItem(R.drawable.baseline_logout_24, "sản phẩm 1", 123, 2123, 1));
+        cartItemsList.add(new CartItem(R.drawable.baseline_logout_24, "sản phẩm 2", 1223, 21323, 12));
+
+        return cartItemsList;
+    }
     private void setControl() {
         btnThanhToanHoaDon = findViewById(R.id.btnThanhToanHoaDon);
+        tongTientxt = findViewById(R.id.totalPriceTxt);
+        cartItemsList = createMockup();
+
+        int total = 0;
+        for (int i = 0; i < cartItemsList.size(); i++) {
+            total+= (cartItemsList.get(i).getInitPrice() * cartItemsList.get(i).getAmout());
+        }
+        tongTientxt.setText(String.format("%,d",total));
+
+        lv = findViewById(R.id.CartLV);
+
+        cartArrayAdapter = new CartArrayAdapter(CartListActivity.this, R.layout.viewholder_cart, cartItemsList);
+
+        lv.setAdapter(cartArrayAdapter);
     }
 
     @Override
--- app/src/main/java/com/example/gearshop/ui/giohang/OrderListActivity.java
+++ app/src/main/java/com/example/gearshop/ui/giohang/OrderListActivity.java
@@ -9,13 +9,23 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.gearshop.MainActivity;
 import com.example.gearshop.R;
+import com.example.gearshop.ui.cart.CartArrayAdapter;
+import com.example.gearshop.ui.cart.CartItem;
+
+import java.util.ArrayList;
 
 public class OrderListActivity extends AppCompatActivity {
     Button btnDatHang;
     Toolbar toolbar;
+    TextView totalPriceNoShipTxt, shipPriceTxt, totalPriceTxt;
+    ArrayList<CartItem> cartItemsList;
+    CartArrayAdapter cartArrayAdapter;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +47,14 @@ public class OrderListActivity extends AppCompatActivity {
             }
         });
     }
+    private ArrayList<CartItem> createMockup () {
+        cartItemsList = new ArrayList<>();
 
+        cartItemsList.add(new CartItem(R.drawable.baseline_logout_24, "sản phẩm 1", 123, 2123, 1));
+        cartItemsList.add(new CartItem(R.drawable.baseline_logout_24, "sản phẩm 2", 1223, 21323, 12));
+
+        return cartItemsList;
+    }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -50,5 +67,23 @@ public class OrderListActivity extends AppCompatActivity {
 
     private void setControl() {
         btnDatHang = findViewById(R.id.btnDatHang);
+        totalPriceNoShipTxt = findViewById(R.id.totalPriceNoShipTxt);
+        shipPriceTxt= findViewById(R.id.shipPriceTxt);
+        totalPriceTxt = findViewById(R.id.totalPriceTxt);
+
+        lv = findViewById(R.id.orderLv);
+        cartItemsList = createMockup();
+
+        int total = 0;
+        for (int i = 0; i < cartItemsList.size(); i++) {
+            total+= (cartItemsList.get(i).getInitPrice() * cartItemsList.get(i).getAmout());
+        }
+        totalPriceNoShipTxt.setText(String.format("%,d",total));
+        shipPriceTxt.setText("25,000");
+        totalPriceTxt.setText((String.format("%,d", total + 25000)));
+
+        cartArrayAdapter = new CartArrayAdapter(OrderListActivity.this, R.layout.viewholder_cart, cartItemsList);
+
+        lv.setAdapter(cartArrayAdapter);
     }
 }
